41bff713ae70bf0407d5854e7309b5c7652e4536
alkn203 <alkn203@gmail.com>
2021-12-05 23:35:41 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const jsdoc = require('gulp-jsdoc3');
-// ファイル結合タスクを作成
-gulp.task('concat', function (done) {
+// ソースファイル結合タスク
+const concatSrc = function (done) {
   // 結合元のファイルを指定
   gulp.src(['src/accessory/*.js', 'src/display/typedlabel.js', 'src/effect/*.js', 'src/ui/*.js', 'src/util/map.js'])
       // 結合後のファイル名を指定
       .pipe(concat('phina-extensions.js'))
       // 出力フォルダを指定
       .pipe(gulp.dest('./build/'));
-  // コールバック
+  // 終了宣言
   done();
-});
+};
 // JSdoc作成タスク
-gulp.task('doc', function (cb) {
-    gulp.src(['README.md', './src/**/*.js'], {read: false})
-        .pipe(jsdoc(cb));
-});
-
-gulp.task('default', gulp.parallel('concat', 'doc'));
+const createDocs = function (cb) {
+  gulp.src(['README.md', './build/phina-extensions.js'], {read: false})
+      .pipe(jsdoc(cb));
+};
+// タスク実行
+exports.default = gulp.series(concatSrc, createDocs);
