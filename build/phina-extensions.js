f776f4382a561f2df5266e8cc39ac47fc3c60b3c
alkn203 <alkn203@gmail.com>
2021-12-12 01:02:48 +0900

    update

--- build/phina-extensions.js
+++ build/phina-extensions.js
@@ -342,42 +342,6 @@ phina.namespace(function() {
     }
   });
 });
-phina.namespace(function() {
-
-  /**
-   * @class phina.effect.Stump
-   */
-  phina.define('phina.effect.Stump', {
-    /**
-     * @constructor
-     */
-    init: function(target, options) {
-      options = ({}).$safe(options || {}, phina.effect.Stump.defaults);
-      // 親クラス初期化
-      this.superInit();
-      // 潰れる速さ
-      var speed = options.speed;
-      // origin変更
-      target.setOrigin(0.5, 1.0);
-      // 位置調整
-      target.y += target.height / 2;
-      // 縦方向に縮小
-      target.tweener.to({scaleY: 0.1}, speed)
-                    .call(function() {
-                      // 削除
-                      target.remove();
-                      this.remove();
-                    }, this)
-                    .play();
-    },
-    
-    _static: {
-      defaults: {
-        speed: 200
-      },
-    }
-  });
-});
 
 phina.namespace(function() {
   /*
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+
+const target = ['src/accessory/aim.js',
+                'src/accessory/collider.js',
+                'src/accessory/gridlayout.js',
+                'src/accessory/ringlayout.js',
+                'src/display/typedlabel.js', 
+                'src/effect/break.js',
+                'src/ui/circlegauge.js',
+                'src/ui/heartgauge.js',
+                'src/ui/timergauge.js',
+                'src/util/map.js'];
 // ソースファイル結合タスク
 const concatSrc = function (done) {
   // 結合元のファイルを指定
-  gulp.src(['src/accessory/*.js', 'src/display/typedlabel.js', 'src/effect/*.js', 'src/ui/*.js', 'src/util/map.js'])
+  gulp.src(target)
       // 結合後のファイル名を指定
       .pipe(concat('phina-extensions.js'))
       // 出力フォルダを指定
