7eede4e82717b353a1b3d9b161da71242e678289
alkn203 <alkn203@gmail.com>
2021-12-26 00:35:37 +0900

    update

--- src/accessory/collider.js
+++ src/accessory/collider.js
@@ -1,17 +1,20 @@
-/*
- * collider
- */
 phina.namespace(function() {
   /**
+   * アタッチされたオブジェクトに指定されたターゲットの方向を常に向かせるアクセサリ
    * @class phina.accessory.Collider
-   * Collider
+   * @memberOf phina.accessory
    * @extends phina.accessory.Accessory
+   *
+   * @example
+   *
+   * var aim = phina.accessory.Aim().attachTo(enemy);
+   * aim.aimTo(player);
+   *
+   * @param {object} [target] - アタッチ対象
    */
   phina.define('phina.accessory.Collider', {
     superClass: 'phina.accessory.Accessory',
-    /**
-     * @constructor
-     */
+
     init: function(target) {
       this.superInit(target);
     },
@@ -33,27 +36,62 @@ phina.namespace(function() {
         this._collider.remove();
       }
     },
-    // 表示
+    /**
+     * コライダーを表示する
+     * @instance
+     * @memberof phina.accessory.Collider
+     *
+     * @return {object} 自分自身
+     */
     show: function() {
       this._collider.show();
       return this;        
     },
-    // 非表示
+    /**
+     * コライダーを非表示にする
+     * @instance
+     * @memberof phina.accessory.Collider
+     *
+     * @return {object} 自分自身
+     */
     hide: function() {
       this._collider.hide();
       return this;        
     },
-    // 相対位置指定
+    /**
+     * コライダーの位置（相対座標）を指定する
+     * @instance
+     * @memberof phina.accessory.Collider
+     *
+     * @param {number} x - x座標
+     * @param {number} y - y座標
+     * @return {object} 自分自身
+     */
     offset: function(x, y) {
       this._collider.setPosition(x, y);
       return this;
     },
-    // サイズ指定
+    /**
+     * コライダーのサイズを指定する
+     * @instance
+     * @memberof phina.accessory.Collider
+     *
+     * @param {number} width - 横サイズ
+     * @param {number} height - 縦サイズ
+     * @return {object} 自分自身
+     */
     setSize: function(width, height) {
       this._collider.setSize(width, height);
       return this;
     },
-    // 衝突判定
+    /**
+     * 他のコライダーとの当たり判定を行う
+     * @instance
+     * @memberof phina.accessory.Collider
+     *
+     * @param {object} collider - 対象のコライダー
+     * @return {boolean} ヒットしたかどうか
+     */
     hitTest: function(collider) {
       if (!this.target) return;
       // 絶対座標の矩形を計算      
