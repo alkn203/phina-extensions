aa882d468a4ad4eee0f841d675072dff253241ac
alkn203 <alkn203@gmail.com>
2023-02-06 22:21:48 +0900

    update

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+const target = ['src/accessory/aim.js',
+                'src/accessory/collider.js',
+                'src/accessory/gridlayout.js',
+                'src/accessory/ringlayout.js',
+                'src/display/typedlabel.js', 
+                'src/effect/break.js',
+                'src/effect/stump.js',
+                'src/ui/ringgauge.js',
+                'src/ui/heartgauge.js',
+                'src/ui/timergauge.js',
+                'src/util/map.js'];
+
+const buildDest = './build';
+const buildName = 'phina-extensions.js';
+// ソースファイル結合タスク
+const concatSrc = function (done) {
+  // 結合元のファイルを指定
+  gulp.src(target)
+      // 結合後のファイル名を指定
+      .pipe(concat(buildName))
+      // 出力フォルダを指定
+      .pipe(gulp.dest(buildDest));
+  // 終了宣言
+  done();
+};
+// ビルドファイル最小化タスク
+const minifySrc = function (done) {
+  // ファイルを指定
+  gulp.src(buildDest + '/' + buildName)
+      // 最小化
+      .pipe(uglify())
+      // ファイル名変更
+      .pipe(rename({
+        extname: '.min.js'
+      }))
+      // 出力先フォルダを指定
+      .pipe(gulp.dest(buildDest));
+  // 終了宣言
+  done();
+};
+// タスク実行
+exports.default = gulp.series(concatSrc, minifySrc);
---
+++ jsdoc.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+./node_modules/.bin/jsdoc build/phina-extensions.js ./README.md -d ./doc -t node_modules/minami
